c293cbab30dd39701c09df188d77ad713a1dd8a4
Anton Hansson <hansson@google.com>
2018-11-07 16:09:35 +0000

    X00QD: Remove obsolete dex settings
    This block is completely redundant because..
    - WITH_DEXPREOPT := true is the default
    - WITH_DEXPREOPT_PIC is never read
    - DEX_PREOPT_DEFAULT := nostripping has no effect when the
      BOARD_USES_SYSTEM_OTHER_ODEX flag is set to true
    
    Test: make
    Change-Id: I618a1e395ca1a14555cd50eef1a4f1aa079b553d

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,15 +118,6 @@ USE_OPENGL_RENDERER := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-    ifneq ($(TARGET_BUILD_VARIANT),eng)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-        endif
-    endif
-endif
-
 # Filesystem
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
