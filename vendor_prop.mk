c276a4703c17a8eefd2336ca1a9cb0bcc08234b7
Lucas Dupin <dupin@google.com>
2020-03-23 10:55:10 -0700

    X00QD: Default blurs to on on coral and flame
    Bug: 149792636
    Test: make
    Change-Id: Id78b72bd3925db21c4c963298c3684e3b54ba43a
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -165,10 +165,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     sdm.debug.rotator_downscale=1 \
     vendor.display.enable_default_color_mode=1
 
-# Enable blurs, hidden under dev option
+# Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Media
