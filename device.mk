fcabab66904c03156b1471828783a264bf8f3f85
emincangencer <emincange@gmail.com>
2020-05-10 19:44:37 +0300

    X00QD: remove powerhint
    * We don't use it

--- device.mk
+++ device.mk
@@ -354,9 +354,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service-qti
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
-
 # Public Libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
