cea03e9f2fe5c3893a106ee97ea418d93abecfed
emincangencer <emincange@gmail.com>
2020-05-13 01:17:23 +0300

    X00QD: Compile libhwui in perf mode
    Signed-off-by: emincangencer <emincange@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,6 +26,9 @@ DEVICE_PATH := device/asus/X00QD
 
 BOARD_VENDOR := asus
 
+# Compile libhwui in performance mode
+HWUI_COMPILE_FOR_PERF := true
+
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := sdm636
 TARGET_NO_BOOTLOADER := true
