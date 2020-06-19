97d3a098874efa0595b40f8b59ba5d55bafa6000
emincangencer <emincange@gmail.com>
2020-06-19 21:39:28 +0300

    X00QD: Updates from V17.0615.2005.25
    Signed-off-by: emincangencer <emincange@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -194,7 +194,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 BOARD_SECCOMP_POLICY := $(DEVICE_PATH)/seccomp
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-03-05
+VENDOR_SECURITY_PATCH := 2020-05-05
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
