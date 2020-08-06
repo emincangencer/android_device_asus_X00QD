40a1c082f197f2e6d203a00547381a4a663ad1fb
emincangencer <emincange@gmail.com>
2020-08-06 22:17:12 +0200

    Revert "X00QD: ZenParts: Add dolby atmos disable switch"
    This reverts commit 2c4e57fa2c85cce200effd90c8417e4685c7ebf0.

--- ZenParts/src/com/asus/zenparts/DeviceSettings.java
+++ ZenParts/src/com/asus/zenparts/DeviceSettings.java
@@ -18,7 +18,6 @@ package com.asus.zenparts;
 
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -29,7 +28,8 @@ import androidx.preference.PreferenceManager;
 import androidx.preference.SwitchPreference;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceCategory;
-import androidx.preference.TwoStatePreference;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.asus.zenparts.kcal.KCalSettingsActivity;
@@ -66,7 +66,6 @@ public class DeviceSettings extends PreferenceFragment implements
     public static final String PREF_SPECTRUM = "spectrum";
     public static final String SPECTRUM_SYSTEM_PROPERTY = "persist.spectrum.profile";
 
-    private static final String KEY_ENABLE_DOLBY_ATMOS = "enable_dolby_atmos";
     public static final String PREF_ENABLE_DIRAC = "dirac_enabled";
     public static final String PREF_HEADSET = "dirac_headset_pref";
     public static final String PREF_PRESET = "dirac_preset_pref";
@@ -112,7 +111,6 @@ public class DeviceSettings extends PreferenceFragment implements
     private Preference mKcal;
     private SecureSettingListPreference mSPECTRUM;
     private Preference mAmbientPref;
-    private static TwoStatePreference mEnableDolbyAtmos;
     private SecureSettingSwitchPreference mEnableDirac;
     private SecureSettingListPreference mHeadsetType;
     private SecureSettingListPreference mPreset;
@@ -220,9 +218,6 @@ public class DeviceSettings extends PreferenceFragment implements
                 enhancerEnabled = false;
             }
         }
-
-        mEnableDolbyAtmos = (TwoStatePreference) findPreference(KEY_ENABLE_DOLBY_ATMOS);
-        mEnableDolbyAtmos.setOnPreferenceChangeListener(this);
 
         mEnableDirac = (SecureSettingSwitchPreference) findPreference(PREF_ENABLE_DIRAC);
         mEnableDirac.setOnPreferenceChangeListener(this);
@@ -369,26 +364,6 @@ public class DeviceSettings extends PreferenceFragment implements
                   setSelinuxEnabled(mSelinuxMode.isChecked(), (Boolean) value);
                   return true;
                 }
-
-            case KEY_ENABLE_DOLBY_ATMOS:
-                  if (preference == mEnableDolbyAtmos) {
-                  boolean enable = (Boolean) value;
-                  Intent daxService = new Intent();
-                  ComponentName name = new ComponentName("com.dolby.daxservice", "com.dolby.daxservice.DaxService");
-                  daxService.setComponent(name);
-                  if (enable) {
-                      // enable service component and start service
-                      this.getContext().getPackageManager().setComponentEnabledSetting(name, 
-                              PackageManager.COMPONENT_ENABLED_STATE_DEFAULT, 0);
-                      this.getContext().startService(daxService);
-                  } else {
-                      // disable service component and stop service
-                      this.getContext().stopService(daxService);
-                      this.getContext().getPackageManager().setComponentEnabledSetting(name,
-                              PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
-                  }
-                  return true;
-              }
 
                 break;
 
