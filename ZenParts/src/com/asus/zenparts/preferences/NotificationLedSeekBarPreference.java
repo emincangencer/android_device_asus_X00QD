4fc90eaa27f8494ac07f560b0536c976e40e6787
emincangencer <emincange@gmail.com>
2020-08-08 15:39:34 +0200

    X00QD: Zenparts: Remove blue led for non-RGB leds

--- ZenParts/src/com/asus/zenparts/preferences/NotificationLedSeekBarPreference.java
+++ ZenParts/src/com/asus/zenparts/preferences/NotificationLedSeekBarPreference.java
@@ -7,7 +7,6 @@ import com.asus.zenparts.FileUtils;
 
 public class NotificationLedSeekBarPreference extends SecureSettingCustomSeekBarPreference {
 
-    public static final String NOTIF_LED_BLUE_PATH = "/sys/class/leds/blue/brightness";
     public static final String NOTIF_LED_RED_PATH = "/sys/class/leds/red/brightness";
     public static final String NOTIF_LED_GREEN_PATH = "/sys/class/leds/green/brightness";
 
@@ -18,14 +17,12 @@ public class NotificationLedSeekBarPreference extends SecureSettingCustomSeekBar
     @Override
     public void onStartTrackingTouch(SeekBar seekBar) {
         notifyChanged();
-        FileUtils.setValue(NOTIF_LED_BLUE_PATH, 64);
         FileUtils.setValue(NOTIF_LED_RED_PATH, 64);
         FileUtils.setValue(NOTIF_LED_GREEN_PATH, 64);
     }
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
         notifyChanged();
-        FileUtils.setValue(NOTIF_LED_BLUE_PATH, 0);
         FileUtils.setValue(NOTIF_LED_RED_PATH, 0);
         FileUtils.setValue(NOTIF_LED_GREEN_PATH, 0);
     }
