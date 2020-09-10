0973a78f71863c7c143ae7e2e719ee4adaf0b0b1
Joey Huab <joey@evolution-x.org>
2020-09-10 18:43:31 +0530

    X00QD: Update to Coral's Sept 2020 fingerprint

--- aosp_X00QD.mk
+++ aosp_X00QD.mk
@@ -51,10 +51,10 @@ EVO_SUPPORT_URL := https://t.me/EvolutionXX00QD
 CUSTOM_BUILD_TYPE := OFFICIAL
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="coral-user 10 QQ3A.200805.001 6578210 release-keys"
+    PRIVATE_BUILD_DESC="coral-user 11 RP1A.200720.009 6720564 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:11/RP1A.200720.009/6720564:user/release-keys
 
 # The following system and vendor props will be set by vendor init
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := \
