7bf15f98454ad572245d870502625dee82f43915
ix5 <ix5@users.noreply.github.com>
2020-09-19 14:16:08 +0530

    X00QD: Add protobuf vendorcompat lib
    Our blobs are compiled against libprotobuf-full-cpp.so, but
    starting in R, Android is using a versioned naming approach,
    e.g. libprotobuf-cpp-full-3.9.1.so.
    
    See https://r.android.com/1109518
    
    The entry in public.libraries.txt is needed to allow the
    linker to find and use the lib outside the VNDK.
    
    See https://source.android.com/devices/tech/config/namespaces_libraries
    
    [wight554: added libprotobuf-cpp-lite as well]
    
    Change-Id: Id1547271a13981600a472b1d100139db787d4ef9

--- device.mk
+++ device.mk
@@ -409,6 +409,7 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-lite-vendorcompat
 
 # Public Libraries
+# See https://source.android.com/devices/tech/config/namespaces_libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
@@ -448,7 +449,6 @@ PRODUCT_PACKAGES += \
     rild \
     librmnetctl \
     libxml2 \
-    libprotobuf-cpp-full \
     qti-telephony-hidl-wrapper \
     qti_telephony_hidl_wrapper.xml \
     qti-telephony-utils \
@@ -533,15 +533,19 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+
 # VR
 PRODUCT_PACKAGES += \
     android.hardware.vr@1.0-impl \
     android.hardware.vr@1.0-service \
     vr.sdm660
 
-# Wallpapers
+# VNDK
+# FIXME: master: compat for libprotobuf
+# See https://android-review.googlesource.com/c/platform/prebuilts/vndk/v28/+/1109518
 PRODUCT_PACKAGES += \
-    PixelLiveWallpaperPrebuilt
+    libprotobuf-cpp-full-vendorcompat \
+    libprotobuf-cpp-lite-vendorcompat
 
 # WFD
 PRODUCT_PACKAGES += \
