bc5a0874de4d4aac3fe8218c6b4dbebf49ddc055
emincangencer <emincange@gmail.com>
2020-10-24 04:11:36 +0300

    X00QD: initialize Lineage
    Signed-off-by: emincangencer <emincange@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,8 +15,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/potato_X00QD.mk
+    $(LOCAL_DIR)/lineage_X00QD.mk
 
 COMMON_LUNCH_CHOICES := \
-    potato_X00QD-userdebug \
-    potato_X00QD-eng
+    lineage_X00QD-userdebug \
+    lineage_X00QD-eng
