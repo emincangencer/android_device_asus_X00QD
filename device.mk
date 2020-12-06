1144ea64d8903d269def687bbca11415a86b81bd
sbwml <984419930@qq.com>
2020-12-06 15:01:17 +0800

    X00QD: Update portion IMS blobs from LA.UM.9.6.2.r1-02500-89xx.0
    - com.qualcomm.qti.uceservice 2.2
    - vendor.qti.imsrtpservice 3.0
    - camera.provider 2.6
    
    Change-Id: I69d3db2d45422231ad40480a6e7c01b40b82e2f8

--- device.mk
+++ device.mk
@@ -174,7 +174,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.6 \
     vendor.qti.hardware.camera.device@1.0 \
     Snap
 
