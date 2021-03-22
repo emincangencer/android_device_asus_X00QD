fce1632f0769153e7071151912ed1ecdadb97af8
Stallix <joey@evolution-x.org>
2021-03-22 19:47:44 +0530

    X00QD: Make blur optional
    Signed-off-by: pkm774 <mprabhat774@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -172,7 +172,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
-    ro.sf.blurs_are_expensive=1
+    ro.sf.blurs_are_expensive=1 \
+    persist.sys.sf.disable_blurs=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
