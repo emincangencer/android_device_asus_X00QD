093e234cf5c9cc2a224cf4b753f39270e91b2170
fakeyatogod <fakeyatokami@gmail.com>
2021-04-06 19:22:25 +0530

    X00QD: Build minijail service for imsrtpservice - Also build its vendor variant
    Change-Id: Idcce131bfd8e02480558d66f2dae22520f00a3dd

--- device.mk
+++ device.mk
@@ -347,6 +347,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
+    
+PRODUCT_PACKAGES += \
+    libavservices_minijail \
+    libavservices_minijail.vendor
 
 # Media Extensions
 PRODUCT_PACKAGES += \
