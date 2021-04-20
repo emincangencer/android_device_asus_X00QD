69f0e25cf4f384ba87e73af215992fdc6f7334f6
limoniumstatice <strangeholic@gmail.com>
2021-04-20 23:49:16 +0530

    X00QD: Add priv-app Permissions for GMS
    Signed-off-by: MLZ94 <zugutoroberto@gmail.com>

--- device.mk
+++ device.mk
@@ -592,4 +592,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
+# GMS priv-app permissions
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-google.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-google.xml
+
 $(call inherit-product, vendor/asus/X00QD/X00QD-vendor.mk)
