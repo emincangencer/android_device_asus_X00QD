82326a05cc2ffe1e79e3df161eebe0cc74652004
henricaodopao <allricknation@gmail.com>
2021-06-08 04:26:58 +0000

    X00QD: build needed camera hals

--- device.mk
+++ device.mk
@@ -165,10 +165,13 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.frameworks.displayservice@1.0_32 \
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.common@1.0 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    libxml2 \
+    android.hardware.camera.provider@2.5 \
+    vendor.qti.hardware.camera.device@1.0 \
     Snap
 
 # Connectivity Engine support (CNE)
