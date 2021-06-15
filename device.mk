180b63ab68b50986824732ba3c716fe9ac6af672
clarencelol <clarencekuiek@icloud.com>
2021-06-15 21:45:26 +0600

    X00QD: Switch to Thermal 2.0 mock * QTI Thermal couldn't read the temperature for some reason

--- device.mk
+++ device.mk
@@ -500,7 +500,7 @@ PRODUCT_PACKAGES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-     android.hardware.thermal@2.0-service.qti
+     android.hardware.thermal@2.0-service.mock
 
 # Thermal
 PRODUCT_COPY_FILES += \
