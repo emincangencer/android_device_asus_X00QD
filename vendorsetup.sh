017c44578954d119bcce5c63d73c0c446c72ead4
henricaodopao <allricknation@gmail.com>
2021-06-25 21:46:26 +0000

    X00QD: track proton clang and display hal needed for wfd

---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Proton Clang
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
+
+# Display Hal (so WFD can work)
+rm -rf hardware/qcom-caf/msm8998/display
+git clone https://github.com/AOSP-11/hardware_qcom-caf_display_msm8998 hardware/qcom-caf/msm8998/display
