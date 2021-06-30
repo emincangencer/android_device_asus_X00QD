bd497441d782e2c2ad224841115d7aacc9a61342
henricaodopao <allricknation@gmail.com>
2021-06-30 13:06:22 +0000

    X00QD: add a --depth=1 to proton clang

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,5 @@
 # Proton Clang
-git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton
+git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
 
 # Display Hal (so WFD can work)
 rm -rf hardware/qcom-caf/msm8998/display
