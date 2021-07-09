f7f7205cb54c8ecc2e128d974d95f3023810ed17
ElectroPerf <kunmun.devroms@gmail.com>
2021-07-09 17:33:08 +0200

    X00QD: Track Hals From AOSP-11
    Signed-off-by: ElectroPerf <kunmun.devroms@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,2 @@
 # Proton Clang
 git clone https://github.com/kdrag0n/proton-clang -b master prebuilts/clang/host/linux-x86/clang-proton --depth=1
-
-rm -rf hardware/qcom-caf/msm8998/audio
-git clone https://github.com/pkm774/hardware_qcom-caf_audio_msm8998 -b 11 hardware/qcom-caf/msm8998/audio
-rm -rf hardware/qcom-caf/msm8998/media
-git clone https://github.com/pkm774/hardware_qcom-caf_media_msm8998 -b 11 hardware/qcom-caf/msm8998/media
-rm -rf hardware/qcom-caf/msm8998/display
-git clone https://github.com/AOSP-11/hardware_qcom-caf_display_msm8998 hardware/qcom-caf/msm8998/display
