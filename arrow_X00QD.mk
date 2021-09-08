ce9dea6a7c999095bfe0933cc0c036137bf432d2
emincangencer <emincange@gmail.com>
2021-09-08 13:00:32 +0000

    X00QD: redfin september fp
    Signed-off-by: emincangencer <emincange@gmail.com>

--- arrow_X00QD.mk
+++ arrow_X00QD.mk
@@ -42,10 +42,10 @@ TARGET_VENDOR := asus
 TARGET_VENDOR_PRODUCT_NAME := X00QD
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RQ3A.210905.001 7511028 release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
--- init/init_X00QD.cpp
+++ init/init_X00QD.cpp
@@ -107,8 +107,8 @@ void vendor_check_variant()
     product_name = "ASUS_X00QD";
     product_device = "ASUS_X00QD";
     product_model = "ASUS_X00QD";
-    build_fingerprint = "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys";
-    build_description = "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys";
+    build_fingerprint = "google/redfin/redfin:11/RQ3A.210905.001/7511028:user/release-keys";
+    build_description = "redfin-user 11 RQ3A.210905.001 7511028 release-keys";
 
     // Override props based on values set
     property_override("ro.build.description", build_description);
